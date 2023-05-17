409ef7250994447ad3562e3569c98aea28d3e1bc
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-05-17 19:53:46 +0200

    Class for reader listPlayers

---
+++ OOP_alexmazzotti2/OOP_alexmazzotti2/LogicsFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OOP_alexmazzotti2
+{
+    class LogicsFile
+    {
+        public LogicsFile()
+        {
+            string url = "listacalciatori.txt";
+            char separator = ';';
+            List<String> li = new List<string>();
+            using (StreamReader reader = new StreamReader(url))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parti=line.Split();
+                    parti[0];
+                    parti[1];
+                }
+            }
+        }
+    }
+}
